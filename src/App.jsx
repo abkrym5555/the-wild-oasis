30bacb64c2bc7d0367de4275de06a349a34d98d2
abkrym5555 <momo542004@gmail.com>
2025-09-07 18:42:39 +0300

    feat:make global styles with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: var(--color-brand-600);
+`;
+
 function App() {
-  return <div>hii mohamed</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <H1>hii mohamed</H1>
+      <Button>check in</Button>
+      <Button>check out</Button>
+      <Input placeholder="your name" />
+    </>
+  );
 }
 
 export default App;
