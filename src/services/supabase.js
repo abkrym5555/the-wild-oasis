e5ceb694150b82d10b64bd37b0a2653773d3eba4
abkrym5555 <momo542004@gmail.com>
2025-09-08 17:55:34 +0300

    feat:connect the supbase in our app

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+// npm i --save @supabase/supabase-js
+
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://szvycgnfsngawjoilcun.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6dnljZ25mc25nYXdqb2lsY3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczMzQ1NTAsImV4cCI6MjA3MjkxMDU1MH0.ompLVZQxn6GZCtSw2D3pbkVo-E-KJVyH6UbBdPF6D6A";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
