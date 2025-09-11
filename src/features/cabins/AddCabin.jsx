2959d4bea82c8b055f8adbb4bf45240d6e2bf4f6
abkrym5555 <momo542004@gmail.com>
2025-09-11 22:41:02 +0300

    refactor:convert the modal to compound components

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -4,19 +4,32 @@ import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
-  const [isOpenModel, setisOpenModel] = useState(false);
   return (
-    <div>
-      <Button onClick={() => setisOpenModel((isOpenModel) => !isOpenModel)}>
-        add new cabin
-      </Button>
-      {isOpenModel && (
-        <Modal onClose={() => setisOpenModel(false)}>
-          <CreateCabinForm onClose={() => setisOpenModel(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens={"cabin-form"}>
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name={"cabin-form"}>
+        <CreateCabinForm />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModel, setisOpenModel] = useState(false);
+//   return (
+//     <div>
+//       <Button onClick={() => setisOpenModel((isOpenModel) => !isOpenModel)}>
+//         add new cabin
+//       </Button>
+//       {isOpenModel && (
+//         <Modal onClose={() => setisOpenModel(false)}>
+//           <CreateCabinForm onClose={() => setisOpenModel(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
