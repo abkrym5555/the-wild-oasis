6f0e4089e078f764d4c8191407b1614be12a4bdc
abkrym5555 <momo542004@gmail.com>
2025-09-16 14:54:58 +0300

    feat:build the pagination

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
 import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
   const { isLoading, bookings, error } = useBooking();
@@ -28,6 +29,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={45} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
