7dc8343f462d07f8b0434339ce887f9f20f77997
abkrym5555 <momo542004@gmail.com>
2025-09-20 00:08:19 +0300

    feat:user logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
-import { useNavigate } from "react-router-dom";
+import { replace, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
@@ -10,7 +10,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard");
+      navigate("/dashboard", { replace: true });
     },
     onError: () => toast.error("email or password are not correct"),
   });
