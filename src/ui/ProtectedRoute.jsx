655586ae43982e5d1a87100685603d78eb80a859
abkrym5555 <momo542004@gmail.com>
2025-09-19 22:56:41 +0300

    feat:Authorization and protectd routs

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 function ProtectedRoute({ children }) {
-  return children;
+  const navigate = useNavigate();
+  const { user, isLoading, isAuth } = useUser();
+
+  useEffect(
+    function () {
+      if (!isAuth && !isLoading) navigate("/login");
+    },
+    [isAuth, isLoading, navigate]
+  );
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuth) return children;
 }
 
 export default ProtectedRoute;
